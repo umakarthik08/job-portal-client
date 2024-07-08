3607e27f7e3a853af79653e5b8a9b5ea599c794c
UmaKarthik <umakarthikray@gmail.com>
2024-07-08 12:07:30 +0530

    First message

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
             {
                 path: "edit-job/:id",
                 element: <UpdateJob/>,
-                loader: ({params}) => fetch(`https://job-portal-server-vert.vercel.app/all-jobs/${params.id}`)
+                loader: ({params}) => fetch(`https://job-portal-server-w1dw.onrender.com/all-jobs/${params.id}`)
             },
             {
                 path: "/login",
